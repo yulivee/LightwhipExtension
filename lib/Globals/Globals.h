4d61bddfccccb1bde3dc7df032bca370662b644f
Sandra Schuhmacher <coding@voiding-warranties.de>
2023-03-12 19:46:20 +0100

    (feat) add hosted web ui to configure Wifi Credentials

---
+++ lib/Globals/Globals.h
@@ -0,0 +1,141 @@
+/**************************************************************
+
+    "iSpindel"
+    All rights reserverd by S.Lang <universam@web.de>
+
+ **************************************************************/
+
+#ifndef _GLOBALS_H
+#define _GLOBALS_H
+
+#pragma once
+//#define USE_DMP false
+#include <Arduino.h>
+#include <Hash.h>
+
+#include <Ticker.h>
+
+//#include <I2Cdev.h>
+//#include <MPU6050.h>
+extern Ticker flasher;
+
+// defines go here
+#define FIRMWAREVERSION "7.3.2"
+
+#define API_FHEM true
+#define API_UBIDOTS true
+#define API_GENERIC true
+#define API_TCONTROL true
+#define API_INFLUXDB true
+#define API_PROMETHEUS true
+#define API_MQTT true
+#define API_THINGSPEAK true
+#define API_BLYNK true
+#define API_BREWBLOX true
+#define API_MQTT_HASSIO true
+#define API_AWSIOTMQTT true //AWS
+#define API_BRICKS true
+
+//#define BLYNK_DEBUG
+//#define APP_DEBUG
+//#define BLYNK_PRINT Serial
+#define BLYNK_NO_BUILTIN
+#define BLYNK_NO_FANCY_LOGO
+#define BLYNK_MAX_SENDBYTES 1200
+
+#define TEMP_CELSIUS 0
+#define TEMP_FAHRENHEIT 1
+#define TEMP_KELVIN 2
+
+#ifndef DEBUG
+#define DEBUG true
+#endif
+
+#define CONSOLE(...)                                                                                                   \
+  do                                                                                                                   \
+  {                                                                                                                    \
+    Serial.print(__VA_ARGS__);                                                                                         \
+  } while (0)
+#define CONSOLELN(...)                                                                                                 \
+  do                                                                                                                   \
+  {                                                                                                                    \
+    Serial.println(__VA_ARGS__);                                                                                       \
+  } while (0)
+
+#define PORTALTIMEOUT 300
+
+#define ADCDIVISOR 191.8
+#define ONE_WIRE_BUS D6 // DS18B20 on ESP pin12
+#define OW_PINS                                                                                                        \
+  (const uint8_t[])                                                                                                    \
+  {                                                                                                                    \
+    D1, D6                                                                                                             \
+  }
+#define RESOLUTION 12 // 12bit resolution == 750ms update rate
+#define OWinterval (760 / (1 << (12 - RESOLUTION)))
+#define CFGFILE "/config.json"
+#define TKIDSIZE 40
+#define DNSSIZE 253
+
+#define MEDIANROUNDSMAX 49
+#define MEDIANROUNDSMIN 29
+#define MEDIANAVRG MEDIANROUNDSMIN
+#define MEDIAN_MAX_SIZE MEDIANROUNDSMAX
+
+#define CBP_ENDPOINT "/api/hydrometer/v1/data"
+
+#define DTUbiDots 0
+#define DTThingspeak 1
+#define DTCraftBeerPi 2
+#define DTHTTP 3
+#define DTTcontrol 4
+#define DTFHEM 5
+#define DTTCP 6
+#define DTiSPINDELde 7
+#define DTInfluxDB 8
+#define DTPrometheus 9
+#define DTMQTT 10
+#define DTTHINGSPEAK 11
+#define DTBLYNK 12
+#define DTBREWBLOX 13
+#define DTAWSIOTMQTT 14 //AWS
+#define DTHTTPS 15
+#define DTBRICKS 16
+
+// Number of seconds after reset during which a
+// subseqent reset will be considered a double reset.
+#define DRD_TIMEOUT 1
+// RTC Memory Address for the DoubleResetDetector to use
+#define DRD_ADDRESS 0
+
+#define WIFIENADDR 1
+#define RTCVALIDFLAG 0xCAFEBABE
+
+// sleep management
+#define RTCSLEEPADDR 5
+#define MAXSLEEPTIME 3600UL //TODO
+#define EMERGENCYSLEEP (myData.sleeptime * 3 < MAXSLEEPTIME ? MAXSLEEPTIME : myData.sleeptime * 3)
+#define LOWBATT 3.3
+
+#define UNINIT 0
+
+extern int16_t ax, ay, az;
+extern float Volt, Temperatur, Tilt, Gravity;
+
+// extern MPU6050 accelgyro;
+extern bool saveConfig();
+extern bool saveConfig(int16_t Offset[6]);
+extern bool formatLittleFS();
+extern void flash();
+
+struct iData
+{
+  char name[21] = "";
+  String ssid;
+  String psk;
+  uint32_t universe = 0;
+};
+
+extern iData whipConfigData;
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
+#include "Globals.h"
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include "DoubleResetDetector.h" // https://github.com/datacute/DoubleResetDetector
 #include <ArtnetWifi.h>
+#include "WiFiManagerKT.h"
 #include "config.h"
+#include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson
+#include <FS.h>          //this needs to be first
+#include <LittleFS.h>
 
 // Art-Net settings
 ArtnetWifi artnet;
@@ -9,6 +15,10 @@ ArtnetWifi artnet;
 int pwmPinRed = D7;
 int pwmPinGreen = D6;
 int pwmPinBlue = D5;
+
+bool shouldSaveConfig = false;
+DoubleResetDetector drd(DRD_TIMEOUT, DRD_ADDRESS);
+iData whipConfigData;
 
 // connect to wifi – returns true if successful or false if not
 boolean ConnectWifi(void)
@@ -50,6 +60,200 @@ boolean ConnectWifi(void)
   return state;
 }
 
+bool formatLittleFS()
+{
+  CONSOLE(F("\nneed to format LittleFS: "));
+  LittleFS.end();
+  LittleFS.begin();
+  CONSOLELN(LittleFS.format());
+  return LittleFS.begin();
+}
+
+bool saveConfig()
+{
+  CONSOLE(F("saving config...\n"));
+
+  // if LittleFS is not usable
+  if (!LittleFS.begin())
+  {
+    Serial.println("Failed to mount file system");
+    if (!formatLittleFS())
+    {
+      Serial.println("Failed to format file system - hardware issues!");
+      return false;
+    }
+  }
+
+  DynamicJsonDocument doc(2048);
+
+  doc["Name"] = whipConfigData.name;
+  doc["universe"] = whipConfigData.universe;
+  // first reboot is for test
+  doc["SSID"] = WiFi.SSID();
+  doc["PSK"] = WiFi.psk();
+
+  File configFile = LittleFS.open(CFGFILE, "w");
+  if (!configFile)
+  {
+    CONSOLELN(F("failed to open config file for writing"));
+    LittleFS.end();
+    return false;
+  }
+  else
+  {
+    serializeJson(doc, configFile);
+#ifdef DEBUG
+    serializeJson(doc, Serial);
+#endif
+    configFile.flush();
+    configFile.close();
+    LittleFS.gc();
+    LittleFS.end();
+    CONSOLELN(F("\nsaved successfully"));
+    return true;
+  }
+}
+// callback notifying us of the need to save config
+void saveConfigCallback()
+{
+  shouldSaveConfig = true;
+}
+
+String htmlencode(String str)
+{
+  String encodedstr = "";
+  char c;
+
+  for (uint16_t i = 0; i < str.length(); i++)
+  {
+    c = str.charAt(i);
+
+    if (isalnum(c))
+    {
+      encodedstr += c;
+    }
+    else
+    {
+      encodedstr += "&#";
+      encodedstr += String((uint8_t)c);
+      encodedstr += ';';
+    }
+  }
+  return encodedstr;
+}
+
+void validateInput(const char *input, char *output)
+{
+  String tmp = input;
+  tmp.trim();
+  tmp.replace(' ', '_');
+  tmp.toCharArray(output, tmp.length() + 1);
+}
+
+bool readConfig()
+{
+  CONSOLE(F("mounting FS..."));
+
+  if (!LittleFS.begin())
+  {
+    CONSOLELN(F(" ERROR: failed to mount FS!"));
+    return false;
+  }
+  else
+  {
+    CONSOLELN(F(" mounted!"));
+    if (!LittleFS.exists(CFGFILE))
+    {
+      CONSOLELN(F("ERROR: failed to load json config"));
+      return false;
+    }
+    else
+    {
+      // file exists, reading and loading
+      CONSOLELN(F("reading config file"));
+      File configFile = LittleFS.open(CFGFILE, "r");
+      if (!configFile)
+      {
+        CONSOLELN(F("ERROR: unable to open config file"));
+      }
+      else
+      {
+        size_t size = configFile.size();
+        DynamicJsonDocument doc(size * 3);
+        DeserializationError error = deserializeJson(doc, configFile);
+        if (error)
+        {
+          CONSOLE(F("deserializeJson() failed: "));
+          CONSOLELN(error.c_str());
+        }
+        else
+        {
+          if (doc.containsKey("Name"))
+            strcpy(whipConfigData.name, doc["Name"]);
+          if (doc.containsKey("SSID"))
+            whipConfigData.ssid = (const char *)doc["SSID"];
+          if (doc.containsKey("PSK"))
+            whipConfigData.psk = (const char *)doc["PSK"];
+          if (doc.containsKey("universe"))
+            whipConfigData.universe = doc["universe"];
+          CONSOLELN(F("parsed config:"));
+#ifdef DEBUG
+          serializeJson(doc, Serial);
+          CONSOLELN();
+#endif
+        }
+      }
+    }
+  }
+  return true;
+}
+
+bool startConfiguration()
+{
+
+  WiFiManager wifiManager;
+
+  wifiManager.setConfigPortalTimeout(PORTALTIMEOUT);
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+  wifiManager.setBreakAfterConfig(true);
+
+  //WiFiManagerParameter custom_name("name", "Lightwhip Name", htmlencode(whipConfigData.name).c_str(), TKIDSIZE);
+  WiFiManagerParameter custom_universe("universe", "ArtNet Universe Number", String(whipConfigData.universe).c_str(), 6, TYPE_NUMBER);
+  //wifiManager.addParameter(&custom_name);
+  wifiManager.addParameter(&custom_universe);
+
+  wifiManager.setConfHostname(htmlencode(whipConfigData.name));
+  wifiManager.setConfSSID(htmlencode(whipConfigData.ssid));
+  wifiManager.setConfPSK(htmlencode(whipConfigData.psk));
+
+  CONSOLELN(F("started Portal"));
+  static char ssid[33] = {0}; // 32 char max for SSIDs
+  if (strlen(whipConfigData.name) == 0)
+  {
+    snprintf(ssid, sizeof ssid, "Lightwhip_%06X", ESP.getChipId());
+    snprintf(whipConfigData.name, sizeof ssid, "Lightwhip_%06X", ESP.getChipId());
+  }
+  else
+  {
+    snprintf(ssid, sizeof ssid, "Lightwhip_%s", whipConfigData.name);
+    WiFi.hostname(whipConfigData.name); // Set DNS hostname
+  }
+
+  wifiManager.startConfigPortal(ssid);
+  whipConfigData.universe = String(custom_universe.getValue()).toInt();
+
+  // save the custom parameters to FS
+  if (shouldSaveConfig)
+  {
+    // Wifi config
+    WiFi.setAutoConnect(true);
+    WiFi.setAutoReconnect(true);
+
+    return saveConfig();
+  }
+  return false;
+}
+
 void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t *data)
 {
   // read universe and put into the right part of the display buffer
@@ -69,19 +273,156 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t *d
   analogWrite(pwmPinBlue, blue);
 }
 
+bool shouldStartConfig(bool validConf)
+{
+
+  // we make sure that configuration is properly set and we are not woken by
+  // RESET button
+  // ensure this was called
+
+  rst_info *_reset_info = ESP.getResetInfoPtr();
+  uint8_t _reset_reason = _reset_info->reason;
+
+  // The ESP reset info is sill buggy. see http://www.esp8266.com/viewtopic.php?f=32&t=8411
+  // The reset reason is "5" (woken from deep-sleep) in most cases (also after a power-cycle)
+  // I added a single reset detection as workaround to enter the config-mode easier
+  CONSOLE(F("Boot-Mode: "));
+  CONSOLELN(ESP.getResetReason());
+  bool _poweredOnOffOn = _reset_reason == REASON_DEFAULT_RST || _reset_reason == REASON_EXT_SYS_RST;
+  if (_poweredOnOffOn)
+    CONSOLELN(F("power-cycle or reset detected, config mode"));
+
+  bool _dblreset = drd.detectDoubleReset();
+  if (_dblreset)
+    CONSOLELN(F("\nDouble Reset detected"));
+
+  bool _wifiCred = !(whipConfigData.ssid.isEmpty() || whipConfigData.psk.isEmpty());
+
+  if (!_wifiCred)
+    CONSOLELN(F("\nERROR no Wifi credentials"));
+
+  if (validConf && !_dblreset && _wifiCred)
+  {
+    CONSOLELN(F("\nno reset detected, normal mode"));
+    return false;
+  }
+  // config mode
+  else
+  {
+    CONSOLELN(F("\ngoing to Config Mode"));
+    return true;
+  }
+}
+
+bool connectBackupCredentials()
+{
+  WiFi.disconnect();
+  WiFi.mode(WIFI_STA); // suggestion that MQTT connection failures can happen if WIFI mode isn't STA.
+  if (strlen(whipConfigData.name) != 0)
+    WiFi.hostname(whipConfigData.name); // Set DNS hostname
+
+  WiFi.begin(whipConfigData.ssid.c_str(), whipConfigData.psk.c_str());
+  unsigned long startedAt = millis();
+  uint8_t wait = 0;
+  while (!WiFi.isConnected())
+  {
+    delay(300);
+    wait++;
+    CONSOLE(F("."));
+    if (wait > 50)
+      break;
+  }
+  CONSOLELN();
+  auto waited = (millis() - startedAt);
+  CONSOLE(F("   -> waited for "));
+  CONSOLE(waited);
+  CONSOLE(F("ms, result "));
+  CONSOLELN(WiFi.status());
+
+  if (WiFi.status() == WL_DISCONNECTED)
+    return false;
+  else
+    return true;
+}
+
 void setup()
 {
   Serial.begin(115200);
+
+  delay(5000);
+
+  bool validConf = readConfig();
+  if (!validConf)
+    CONSOLELN(F("\nERROR config corrupted"));
+
+  if (shouldStartConfig(validConf))
+  {
+    // rescue if wifi credentials lost because of power loss
+    if (!startConfiguration())
+    {
+      // test if ssid exists
+      if (WiFi.SSID() == "" && whipConfigData.ssid != "" && whipConfigData.psk != "")
+      {
+        connectBackupCredentials();
+      }
+    }
+  }
+
+  // to make sure we wake up with STA but AP
+  WiFi.mode(WIFI_STA);
+  WiFi.persistent(false);
+  WiFi.hostname(whipConfigData.name); 
+  WiFi.begin(whipConfigData.ssid.c_str(), whipConfigData.psk.c_str());
+
+  if (!WiFi.isConnected())
+  {
+    unsigned long startedAt = millis();
+    uint8_t wait = 0;
+    while (!WiFi.isConnected())
+    {
+      delay(300);
+      wait++;
+      CONSOLE(F("."));
+      if (wait > 50)
+        break;
+    }
+    CONSOLELN();
+    auto waited = (millis() - startedAt);
+    CONSOLE(F("After waiting "));
+    CONSOLE(waited);
+    CONSOLE(F("ms, result "));
+    CONSOLELN(WiFi.status());
+  }
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    CONSOLE(F("IP: "));
+    CONSOLELN(WiFi.localIP());
+  }
+  else
+  {
+    CONSOLELN(F("Failed to connect -> trying to restore connection..."));
+    if (connectBackupCredentials())
+      CONSOLE(F("   -> Connection restored!"));
+    else
+    {
+      CONSOLE(F("   -> Failed to restore connection..."));
+      WiFi.disconnect();
+    }
+  }
+
+  CONSOLELN();
+
   ESP.wdtDisable();
   ESP.wdtEnable(1000);
-  ConnectWifi();
+  // ConnectWifi();
 
   pinMode(pwmPinRed, OUTPUT);
   pinMode(pwmPinGreen, OUTPUT);
   pinMode(pwmPinBlue, OUTPUT);
 
-  artnet.begin();
+  // artnet.begin();
   // this will be called for each packet received
+  CONSOLELN(F("Starting ArtNet"));
   artnet.setArtDmxCallback(onDmxFrame);
   artnet.begin();
 }
